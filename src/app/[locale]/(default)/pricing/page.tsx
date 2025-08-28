c58d20dd9d085b3a785dda69cc07a7500df61ca8
coconamo <suhengkusua@163.com>
2025-08-28 11:27:58 +0800

    legal

--- src/app/[locale]/(default)/pricing/page.tsx
+++ src/app/[locale]/(default)/pricing/page.tsx
@@ -1,6 +1,33 @@
 import Pricing from "@/components/blocks/pricing";
 import { getPricingPage } from "@/services/page";
 import FAQ from "@/components/blocks/faq";
+import { getTranslations, setRequestLocale } from "next-intl/server";
+import { Metadata } from "next";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}): Promise<Metadata> {
+  const { locale } = await params;
+  setRequestLocale(locale);
+
+  const t = await getTranslations();
+  
+  let canonicalUrl = `${process.env.NEXT_PUBLIC_WEB_URL}/pricing`;
+  if (locale !== "en") {
+    canonicalUrl = `${process.env.NEXT_PUBLIC_WEB_URL}/${locale}/pricing`;
+  }
+
+  return {
+    title: t("metadata.pricing.title"),
+    description: t("metadata.pricing.description"),
+    keywords: t("metadata.pricing.keywords"),
+    alternates: {
+      canonical: canonicalUrl,
+    },
+  };
+}
 
 export default async function PricingPage({
   params,
